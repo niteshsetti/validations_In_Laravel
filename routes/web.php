66015c150add7d22bdb05f0e1d56fbdec7699651
niteshsetti <nitesh.setti2001@gmail.com>
2021-12-22 21:25:12 +0530

    newcommit

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,11 @@ Route::post('register',[Validation::class,'register_validation']);
 Route::post('dashboard',[Validation::class,'login_validation']);
 /*Route::get('/main', function () {
     return view('main');
-});*/
-/*Route::get('/list', function () {
+});
+Route::get('/list', function () {
     return view('view');
 });*/
-Route::get('/main',function(){
+/*Route::get('/main',function(){
   if(!session()->has('data')){
       return redirect("login")->with('status','Please Login-First');
   }
@@ -45,18 +45,21 @@ Route::get('/login',function(){
     else{
     return view('/login');
     }
-  });
+  });*/
   Route::get('/signout',function(){
     if(session()->has('data')){
        session()->forget('data');
        return redirect('/login');
     }
   });
-  Route::get('/list',function(){
+  /*Route::get('/list',function(){
     if(!session()->has('data')){
         return redirect("login")->with('status','Please Login-First');
     }
     else{
       return view('view');
     }
+  });*/
+  Route::group(['middleware'=>['check']],function(){
+   Route::view('main','main');
   });
